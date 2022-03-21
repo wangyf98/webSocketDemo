b223a1f88b5054ebe2492d2c8651763f553d1f60
wangyongfeng <2973539721@qq.com>
2022-03-21 20:04:57 +0800

    "webSocket in Spring bug"

---
+++ src/main/java/wyf/websocket/WebSocketHandler.java
@@ -0,0 +1,53 @@
+package wyf.websocket;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+@ServerEndpoint("/websocket/test")
+@Service
+public class WebSocketHandler {
+
+    private final  Map<String, WebSocketHandler> sessionMap = new HashMap<>();
+
+    private Session session;
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    @OnOpen
+    public void open(Session session){
+        setSession(session);
+        sessionMap.put(session.getId(), this);
+        System.out.println("size is " + sessionMap.size());
+    }
+
+    @OnClose
+    public void close(Session session){
+        System.out.println("关闭一个连接");
+        sessionMap.remove(session.getId());
+    }
+
+    @OnMessage
+    public void sendMessage(String message){
+        for (Map.Entry<String, WebSocketHandler> sessionEntry : sessionMap.entrySet()) {
+            try {
+                sessionEntry.getValue().session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
